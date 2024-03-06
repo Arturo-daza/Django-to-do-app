3ede1998fe05359cf6f0fb34a8b1acf10c14e26a
TechRider <techrider@gmail.com>
2024-03-05 20:59:18 -0500

    improve the validation of singup form

--- tasks/views.py
+++ tasks/views.py
@@ -5,29 +5,37 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request ):
-    return render(request, 'home.html')
+
+def home(request):
+    return render(request, "home.html")
 
 
-def signup(request ):
+def signup(request):
 
-    if request.method == "GET": 
-        return render(request, 'signup.html', {
-        "form": UserCreationForm
-    })
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm})
 
     else:
         if request.POST["password1"] == request.POST["password2"]:
-            #register user
-            try: 
-                user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+            # register user
+            try:
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
                 user.save()
                 return HttpResponse("User created successfully")
             except:
-                return HttpResponse("User already exists")
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "Username already taken"},
+                )
         else:
-            return HttpResponse("Passwords do not match")
+            return render(
+                request,
+                "signup.html",
+                {"form": UserCreationForm, "error": "password do not match"},
+            )
         print(request.POST)
         print("obteniendo datos")
-
-    
